4591badf59d7742592b4b5c66d82651ea7cae50a
Ron Matthew Alunan <ron.alunan@usjr.edu.ph>
2022-10-08 01:45:12 +0800

    Update 10/8/2022:
    - Added buttons and logo
    - Added login item widget in login form
    - Changed app theme

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,19 @@ class LoginPage extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         //default colors
-        primarySwatch: Colors.pink,
+        colorScheme: const ColorScheme.light().copyWith(primary: const Color(0xffff7582)),
+        scaffoldBackgroundColor: const Color(0xff725a7a),
+        //text colors
+        textTheme: Theme.of(context).textTheme.apply(
+          bodyColor: const Color(0xffff7582), 
+          displayColor: const Color(0xffff7582),
+        ),
         //default fonts
         fontFamily: 'Open Sans',
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('asd to SP Elec'),
+          title: const Text('asdsad to SP Elec'),
         ),
         body: const LoginForm(),
       )
@@ -30,19 +36,38 @@ class LoginForm extends StatelessWidget {
   const LoginForm({super.key});
 
   Widget loginItem(String placeholder) {
-    return Text(placeholder);
+    return TextField(
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: placeholder,
+        prefixIcon: Icon(placeholder == 'Username' ? Icons.account_circle_sharp : Icons.key_sharp),
+      ),
+      obscureText: placeholder == "Password",
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          loginItem("Login"),
-          loginItem("Dont")
-        ],
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png', height: 200, width: 200),
+            loginItem('Username'),
+            loginItem('Password'),
+            OutlinedButton(
+              onPressed: () => {}, 
+              child: const Text('Login'),
+            ),
+            ElevatedButton(
+              onPressed: () => {}, 
+              child: const Text('Login'),
+            )
+          ],
+        ),
       )
     );
   }
